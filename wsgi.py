88382979dc1b0641864ae882d7ce72455bf1f446
ts97783 <tim_stockford@hotmail.com>
2019-06-09 18:10:17 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World, this a test commit!"
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
